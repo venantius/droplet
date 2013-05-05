a8803d30e21c003b81ab861004a3d2888ea6dce7
venantius <william.d.jarvis@gmail.com>
2013-05-05 14:19:14 -0700

    Updated REAME

--- bin/test.py
+++ bin/test.py
@@ -8,9 +8,9 @@ simple_sampler = L0Sampler(20, 2)
 #print simple_sampler
 
 print "\nTESTING UPDATE FUNCTION\n"
-simple_sampler.update(2, 1)
-simple_sampler.update(3, 3)
-simple_sampler.update(4, 5)
+#simple_sampler.update(2, 1)
+#simple_sampler.update(3, 3)
+#simple_sampler.update(4, 5)
 simple_sampler.update(5, 7)
 simple_sampler.update(6, 20)
 simple_sampler.update(7, 45)
@@ -18,12 +18,14 @@ simple_sampler.update(8, 100)
 simple_sampler.update(9, 200)
 simple_sampler.update(10, 500)
 
-for _num in range(1,20):
-    simple_sampler.update(_num, 1)
+#for _num in range(1,20):
+    #    simple_sampler.update(_num, 1)
 
 print simple_sampler
 
 print "\nTESTING RECOVERY FUNCTION:\n"
 
+recovered_weight = simple_sampler.recover()
+print recovered_weight
 print simple_sampler.sample()
 
