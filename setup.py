204f30db874342034fcde076d314e79f691d4a58
venantius <william.d.jarvis@gmail.com>
2013-05-07 12:49:32 -0700

    Preliminary setup.py commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+Setup.py
+"""
+
+from distutils.core import setup
+
+setup(
+        name = "el_zero",
+        version = "0.3.0",
+        description = "A Straightforward L0 Sampler",
+        author = "Venantius",
+        author_email = "root@venanti.us",
+        url = "http://venanti.us",
+        packages = ["el_zero"]
+        )
+
+
+
+
+
